7f6e4848f577c439809df328d70e1f83d82a74c9
Ula <>
2014-07-22 11:22:36 +0200

    Ula: klasy Zamowienie i Promocja

---
+++ src/pl/altkom/ogoreczek/model/PromocjaProcentowa.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2014 Ula.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pl.altkom.ogoreczek.model;
+
+/**
+ *
+ * @author Ula
+ */
+public class PromocjaProcentowa extends Promocja {
+    private double procent;
+
+    public double getProcent() {
+        return procent;
+    }
+
+    public void setProcent(double procent) {
+        this.procent = procent;
+    }
+    
+}
