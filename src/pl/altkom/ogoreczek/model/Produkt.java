b0c83df08d139d68147261fdd26ca7adf55371b9
Ewa <>
2014-07-22 11:23:33 +0200

    dodane gettery, settery

--- src/pl/altkom/ogoreczek/model/Produkt.java
+++ src/pl/altkom/ogoreczek/model/Produkt.java
@@ -31,5 +31,86 @@ public class Produkt {
     private JednostkaMiary jM;
     private Opakowanie opakowanie;
     private double iloscWPaczce;
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public String getKategoria() {
+        return kategoria;
+    }
+
+    public String getGatunek() {
+        return gatunek;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public String getProducent() {
+        return producent;
+    }
+
+    public int getIloscWMagazynie() {
+        return iloscWMagazynie;
+    }
+
+    public Boolean isCzyTrwale() {
+        return czyTrwale;
+    }
+
+    public JednostkaMiary getjM() {
+        return jM;
+    }
+
+    public Opakowanie getOpakowanie() {
+        return opakowanie;
+    }
+
+    public double getIloscWPaczce() {
+        return iloscWPaczce;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public void setKategoria(String kategoria) {
+        this.kategoria = kategoria;
+    }
+
+    public void setGatunek(String gatunek) {
+        this.gatunek = gatunek;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public void setProducent(String producent) {
+        this.producent = producent;
+    }
+
+    public void setIloscWMagazynie(int iloscWMagazynie) {
+        this.iloscWMagazynie = iloscWMagazynie;
+    }
+
+    public void setCzyTrwale(Boolean czyTrwale) {
+        this.czyTrwale = czyTrwale;
+    }
+
+    public void setjM(JednostkaMiary jM) {
+        this.jM = jM;
+    }
+
+    public void setOpakowanie(Opakowanie opakowanie) {
+        this.opakowanie = opakowanie;
+    }
+
+    public void setIloscWPaczce(double iloscWPaczce) {
+        this.iloscWPaczce = iloscWPaczce;
+    }
+    
     
 }
