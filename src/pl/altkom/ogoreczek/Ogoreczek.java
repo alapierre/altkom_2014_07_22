6bc13fa3e1e8191910f44fc44e0640040c84f31b
Adrian Lapierre <adrian@soft-project.pl>
2014-07-23 14:07:34 +0200

    odczyt danych klienta fiormowego

--- src/pl/altkom/ogoreczek/Ogoreczek.java
+++ src/pl/altkom/ogoreczek/Ogoreczek.java
@@ -16,6 +16,8 @@
 
 package pl.altkom.ogoreczek;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -32,12 +34,86 @@ public class Ogoreczek {
     
     public static void main(String[] args) {
         
-        pokazMenu();
-        
         Scanner sc = new Scanner(System.in);
-        int i = czytajLiczbeZKlawiatury(sc);
         
-        System.out.println("wybrałeś: " + i);
+        while(true) {
+            pokazMenu();
+            int i = czytajLiczbeZKlawiatury(sc);        
+            System.out.println("wybrałeś: " + i);
+            
+            switch (i) {
+                case 2:
+                    Klient klient = wczytajDaneKlientaZKlawiatury(sc);
+                    System.out.println(klient);        
+                    break;
+                case 0:
+                    return;
+                    
+            }
+            
+        }
+    }
+    
+    public static Klient wczytajDaneKlientaZKlawiatury(Scanner sc) {
+        
+        System.out.println("Jaki typ klienta:");
+        System.out.println("1. Firmowy");
+        System.out.println("2. Indywidualny");
+        
+        System.out.println("inna wartość = powrót");
+        
+        int res = czytajLiczbeZKlawiatury(sc);
+        
+        switch (res) {
+            case 1:
+                return wczytajKlientaFirmowego(sc);
+            case 2:
+                return wczytajKlientaIndywidualnego(sc);
+            default:
+                return null;
+        }
+    }
+    
+    public static KlientFirmowy wczytajKlientaFirmowego(Scanner sc) {
+        
+        System.out.println("login: ");
+        String login = sc.next();
+        
+        System.out.println("Haslo: ");
+        String haslo = sc.next();
+        System.out.println("Mail: ");
+        String email = sc.next();
+        
+        KlientFirmowy klient = new KlientFirmowy(login, haslo, email, "nowy", new Date());
+        
+        System.out.println("Nazwa: ");
+        klient.setNazwa(sc.next());
+        
+        System.out.println("NIP: ");
+        klient.setNIP(sc.next());
+        
+        System.out.println("DFata powstania firmy: ");
+        
+        try {
+            klient.setDataRejestracjiFirmy(konwertujDate(sc.next()));
+        } catch (ParseException ex) {
+            System.out.println("błędna data");
+        }
+        return klient;
+    }
+    
+    public static Date konwertujDate(String str) throws ParseException {
+        
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        
+        return df.parse(str);
+    }
+    
+    public static KlientIndywidualny wczytajKlientaIndywidualnego(Scanner sc) {
+        
+        KlientIndywidualny klient = new KlientIndywidualny(null, null, null, null, null);
+        return klient;
+        
     }
     
     public static void pokazMenu() {
@@ -46,6 +122,8 @@ public class Ogoreczek {
         System.out.println("2. Dodaj kliena");
         System.out.println("3. Lista zamówień");
         System.out.println("4. Dodaj zamówienie");
+        
+        System.out.println("0. Koniec");
         
     }
     
